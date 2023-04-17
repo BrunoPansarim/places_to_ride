561e2a0fe41640431f4757bd81e8cade02a657c5
BrunoPansarim <bruno.pansarim@gmail.com>
2023-04-17 10:21:21 -0300

    Criar metodo para pegar fotos dentro do aparelho

--- lib/pages/places_form_page.dart
+++ lib/pages/places_form_page.dart
@@ -1,4 +1,7 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
+import 'package:places_to_ride/widgets/image_input.dart';
 
 class PlaceFormPage extends StatefulWidget {
   const PlaceFormPage({super.key});
@@ -8,24 +11,54 @@ class PlaceFormPage extends StatefulWidget {
 }
 
 class _PlaceFormPageState extends State<PlaceFormPage> {
+  final _titleController = TextEditingController();
+
+  void _submited() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Novo Lugar'),
       ),
-      body: const Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'E-mail',
-              labelStyle: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'E-mail',
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                    const SizedBox(height: 10),
+                    const ImageInput(),
+                  ],
+                ),
               ),
             ),
-            textAlign: TextAlign.center,
-          )
+          ),
+          ElevatedButton.icon(
+            onPressed: _submited,
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.greenAccent,
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+          ),
         ],
       ),
     );
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,3 +1,5 @@
+import "dart:io";
+import 'package:image_picker/image_picker.dart';
 import "package:flutter/material.dart";
 
 class ImageInput extends StatefulWidget {
@@ -8,6 +10,20 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    XFile imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    ) as XFile;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -16,10 +32,24 @@ class _ImageInputState extends State<ImageInput> {
           width: 200,
           height: 100,
           foregroundDecoration: BoxDecoration(
-            border: Border.all(width: 1, color: Colors.grey),
+            border: Border.all(width: 2, color: Colors.grey),
           ),
           alignment: Alignment.center,
-          child: const Text('Ainda nada'),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : const Text('Ainda nada'),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: TextButton.icon(
+            icon: const Icon(Icons.camera),
+            label: const Text('Tirar Foto'),
+            onPressed: _takePicture,
+          ),
         ),
       ],
     );
